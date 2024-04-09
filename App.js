50eb973b296b5326470e2f666de3add3823a833c
Md Anwar Alam <mdanwar40212@gmail.com>
2024-04-10 01:51:18 +0530

    second commit

---
+++ App.js
@@ -0,0 +1,42 @@
+const questions=[
+    {
+        'que':' Which of the following language is the mark up language?',
+        'a': 'HTML',
+        'b':'CSS',
+        'c':'JavaScript',
+        'd':'PHP',
+        'correct':'a'
+    },
+    {
+        'que':' Who is the CEO of google?',
+        'a': 'Md Anwar Alam',
+        'b':'Sundar Pichai',
+        'c':'Jos Bezos',
+        'd':'Roman Reing',
+        'correct':'b'
+    },
+    {
+        'que':' Who developed Java?',
+        'a': 'Sarfraz Alam',
+        'b':'naved Malik',
+        'c':'Jems Gauslin',
+        'd':'Md Anwar Alam',
+        'correct':'c'
+    },
+    {
+        'que':' What is the full form of HTML?',
+        'a': 'Hyper Text Markup Language',
+        'b':'Hyper Tension Markup Language',
+        'c':'Hyper Text Machine Language',
+        'd':'Hyper Text Machine Linguist',
+        'correct':'a'
+    },
+    {
+        'que':' Who is the the founder of WWW.?',
+        'a': 'Charlie',
+        'b':'Md Anwar Alam',
+        'c':'Md Aamir',
+        'd':'Tim Berners Lee',
+        'correct':'d'
+    }
+]
